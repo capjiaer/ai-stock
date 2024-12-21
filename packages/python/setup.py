b5777baf0360c351b24a12d34a2ed3cd84333bdf
Anping Chen <capjiaer@163.com>
2024-12-21 19:45:17 +0800

    make sure translate is a formal module

---
+++ packages/python/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='translate',
+    version='0.1',
+    packages=find_packages(),
+    description='A package for translation functionality',
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://your.package.url',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
